a1caf59d12983407f99bd16093017f89384a1785
Kasim ADALAN <kasimadalan@gmail.com>
2019-07-29 16:46:40 +0300

    Initial Commit

---
+++ KisilerUygulamasi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasi
+//
+//  Created by Kasım Adalan on 29.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
